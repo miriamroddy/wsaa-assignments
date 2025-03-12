0a967e8a1c70005713d0fa9801b78fdeeddd1c1a
miriamroddy <miriam.roddy@gmail.com>
2025-03-12 16:52:47 +0000

    More assignment 3 - SSL not working

--- assignment03-cso.py
+++ assignment03-cso.py
@@ -13,3 +13,14 @@ try:
     
     # Check if request was successful (200 means OK)
     if response.status_code == 200:
+                with open("cso.json", "w") as file:
+            # Convert JSON response to a Python object and write to file
+            json.dump(response.json(), file)
+            
+        print("Success! Dataset saved to cso.json")
+    else:
+        print(f"Failed to retrieve CSO data. Code: {response.status_code}")
+
+# Again, we want to catch exceptions
+except requests.exceptions.RequestException as e:
+    print(f"Uh-oh. An error occurred: {e}")
