c414b162ea14eb9feab851de4f50829e44cf66e9
miriamroddy <miriam.roddy@gmail.com>
2025-03-14 08:24:49 +0000

    Finished Assignment 3

--- assignment03-cso.py
+++ assignment03-cso.py
@@ -13,22 +13,23 @@ http = urllib3.PoolManager(cert_reqs='CERT_NONE', assert_hostname=False)
 # URL for CSO dataset:
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
 
-# Try block - helps catch errors
+# try block - was handy when getting errors
 # https://www.w3schools.com/python/python_try_except.asp
 try:
-    # Send GET request
-    response = requests.get(url)
-    
-    # Check if request was successful (200 means OK)
-    if response.status_code == 200:
-                with open("cso.json", "w") as file:
-            # Convert JSON response to a Python object and write to file
-            json.dump(response.json(), file)
-            
+    # send GET request
+    response = http.request('GET', url)
+
+
+    # then check - was request successful? 200 means OK
+    if response.status == 200:
+       
+        with open("cso.json", "w") as file:
+            #  convert JSON object to string and writes to file
+            json.dump(json.loads(response.data.decode('utf-8')), file)
         print("Success! Dataset saved to cso.json")
     else:
-        print(f"Failed to retrieve CSO data. Code: {response.status_code}")
-
-# Again, we want to catch exceptions
-except requests.exceptions.RequestException as e:
+        print(f"Failed to retrieve CAO data. Code: {response.status}")
+       
+# again, we want to catch exceptions
+except Exception as e:
     print(f"Uh-oh. An error occurred: {e}")
